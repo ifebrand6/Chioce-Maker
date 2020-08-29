634d8c30ddb7293861bcd735cdb41a88dd23f907
udeogu ifechuwku <ifebrand6@gmail.com>
2020-08-29 13:59:35 +0100

    footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,11 @@ function date() {
     let time = date.getFullYear()
     return time
 }
+const time = date()
 function Footer(params) {
     return (
         <footer>
-            <h4>Choice maker app {date()} by Udeogu Ifechukwu</h4>
+            <h4>Choice maker app {time} by Udeogu Ifechukwu</h4>
         </footer>
     )
 }
